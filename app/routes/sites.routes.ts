1e764bad7015a88d26aca3093feb52c8859b9781
890Tom <zafi.tommy@gmail.com>
2023-08-06 00:40:57 +0300

    Update sites.routes.ts

--- app/routes/sites.routes.ts
+++ app/routes/sites.routes.ts
@@ -9,7 +9,7 @@ const sitesbase = `${Paths.Base}/${Paths.Site.Base}`;
 
 const allsitepath = `${sitesbase}`;
 const detailpath = `${sitesbase}/:id`;
-const searchpath = `${Paths.Site.Search}/${sitesbase}`
+const searchpath = `${Paths.Base}/${Paths.Site.Search}`
 
 router.get(allsitepath, allsite);
 router.get(detailpath, onesite);
